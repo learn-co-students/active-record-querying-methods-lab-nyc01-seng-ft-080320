0171fdebf58a96c6373df45556c9877313b6488c
anickel101 <anson.nickel@gmail.com>
2020-08-13 10:14:15 -0400

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,39 @@
+require 'pry'
+
+class Show < ActiveRecord::Base
+
+    def self.highest_rating
+        self.maximum(:rating)
+    end
+
+    def self.most_popular_show
+        #self.find_by rating: self.highest_rating
+        #self.where(rating: self.highest_rating).first
+
+        #Below is best practice because we aren't passing variable to database, only to the function.
+        self.where("rating = ?", self.highest_rating).first
+    end
+
+    def self.lowest_rating
+        self.minimum(:rating)
+    end
+
+    def self.least_popular_show
+        self.where("rating = ?", self.lowest_rating).first
+    end
+
+    def self.ratings_sum
+        self.sum(:rating)
+    end
+
+    def self.popular_shows
+        self.where("rating > ?", 5)
+    end
+
+    def self.shows_by_alphabetical_order
+        self.order(:name)
+    end
+
+
+
+end
