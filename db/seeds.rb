6c35378df28a3ec7c9211fbc18c2479f45789aa6
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-14 16:53:50 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Show.destroy_all
+
+Show.create(name: "show1", network: "network1", day: "monday", rating: 10, season: "first")
+Show.create(name: "show1", network: "network1", day: "tuesday", rating: 10, season: "first")
+Show.create(name: "show1", network: "network1", day: "wednesday", rating: 10, season: "first")
+Show.create(name: "show2", network: "network1", day: "thursday", rating: 9, season: "first")
+Show.create(name: "show2", network: "network1", day: "friday", rating: 9, season: "first")
+Show.create(name: "show3", network: "network2", day: "saturday", rating: 10, season: "first")
+Show.create(name: "show3", network: "network2", day: "sunday", rating: 10, season: "first")
+Show.create(name: "show4", network: "network3", day: "monday", rating: 7, season: "second")
+Show.create(name: "show4", network: "network3", day: "tuesday", rating: 7, season: "second")
